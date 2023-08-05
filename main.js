f98d5e25ebe23ab2964dbe6b2eb4e38f2c6c7b44
ZetLink <royalezetlink@gmail.com>
2023-08-05 13:14:21 -0300

    start

---
+++ main.js
@@ -0,0 +1,76 @@
+const { app, BrowserWindow, Menu } = require('electron')
+const path = require('path')
+const { openAboutWindow } = require('about-window').default;
+const contextMenu = require('electron-context-menu');
+const appData = require("./package.json");
+const join = require('path').join;
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  mainWindow.loadFile('index.html')
+  mainWindow.maximize();
+
+  contextMenu({
+    window: mainWindow,
+    prepend: (defaultActions, params, browserWindow) => [],
+    showInspectElement: false,
+  });
+
+  const template = [
+    {
+      label: "File",
+      submenu: [
+        { role: "minimize" },
+        { role: "close" },
+      ],
+    },
+    {
+      label: "View",
+      submenu: [
+        { role: "zoomIn" },
+        { role: "zoomOut" },
+        { role: "resetZoom" },
+      ],
+    },
+    {
+      label: 'Help',
+      submenu: [
+        { label: `About ${appData.productName}`, 
+          click: () =>
+            openAboutWindow({
+              product_name: appData.productName,
+              icon_path: path.join(__dirname, './img/icon.png'), // Ruta del icono de la aplicación
+              copyright: appData.copyright,
+              homepage: 'https://vonsim.github.io', // URL de la página de inicio de la aplicación
+              license: 'GNU', // Licencia de la aplicació
+              win_options: {
+                titleBarStyle: "hidden",
+                resizable: false,
+                open_devtools: false,
+              },
+            }),
+        }
+      ],
+    },
+  ];
+
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+};
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
